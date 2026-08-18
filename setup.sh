3a52b36c4a490fdd448ab91453a0daab35933906
Uwe Kallmeyer <uwe.kallmeyer@gmail.com>
2026-08-18 19:07:53 +0200

    Set up Jekyll site scaffold with Minima theme
    Adds jekyll new default structure, updated Gemfile.lock, and a
    .gitignore covering build artifacts, Bundler, OS and editor files.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# sudo apt update
+# sudo apt install ruby-full build-essential zlib1g-dev
+
+# echo '# Install Ruby Gems to ~/.gems' >> ~/.bash_aliases
+# echo 'export GEM_HOME="$HOME/.gems"' >> ~/.bash_aliases
+# echo 'export PATH="$HOME/.gems/bin:$PATH"' >> ~/.bash_aliases
+# source ~/.bash_aliases
+
+# gem install jekyll bundler
+
+# Danach im Repo-Verzeichnis prüfen/initialisieren:
+
+# jekyll -v
+# bundle -v
+
+# bundle init
+# bundle add jekyll  # oder: bundle add github-pages --group jekyll_plugins
+
